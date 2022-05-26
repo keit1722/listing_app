8fe3cf506ea40c8daa2d7aac1a1d0ccb7e5c0976
keit1722 <keit1722@gmail.com>
2022-05-27 04:49:47 +0900

    [add]Capybaraの設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,9 +3,12 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort('The Rails environment is running in production mode!') if Rails.env.production?
+if Rails.env.production?
+  abort('The Rails environment is running in production mode!')
+end
 require 'rspec/rails'
 require 'factory_bot'
+require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -65,4 +68,5 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
+  config.include SystemHelper, type: :system
 end
