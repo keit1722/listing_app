b2befb16d67435238e4bcafed31c71a5da593f78
keit1722 <keit1722@gmail.com>
2022-05-27 03:18:19 +0900

    [add]not_authenticatedメソッドをオーバーライド

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   add_flash_types :error, :success, :warning
+
+  def not_authenticated
+    redirect_to login_path, error: 'ログインしてください'
+  end
 end
