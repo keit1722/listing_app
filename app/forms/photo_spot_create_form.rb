fa93ecd2ba3eaf8572c8fa66e3efcb160e017bc1
keit1722 <keit1722@gmail.com>
2022-06-10 11:51:12 +0900

    [add]PhotoSpotの新規登録・更新用のフォームオブジェクトを作成

---
+++ app/forms/photo_spot_create_form.rb
@@ -0,0 +1,42 @@
+class PhotoSpotCreateForm
+  include ActiveModel::Model
+
+  DAY_COUNT = 8
+
+  attr_accessor :photo_spot, :district_id
+
+  validates :district_id, presence: true
+
+  def initialize(organization, params = {})
+    @organization = organization
+    super params
+
+    self.photo_spot = organization.photo_spots.build unless photo_spot.present?
+    self.district_id = params[:district_id]
+  end
+
+  def photo_spot_attributes=(attributes)
+    self.photo_spot = @organization.photo_spots.build(attributes)
+  end
+
+  def save
+    build_asscociations
+
+    return false unless valid?
+
+    ActiveRecord::Base.transaction { photo_spot.save! }
+  rescue ActiveRecord::RecordInvalid
+    false
+  end
+
+  private
+
+  def build_asscociations
+    photo_spot.district_ids = district_id.to_i unless district_id.empty?
+  end
+
+  def valid?
+    super
+    photo_spot.valid?
+  end
+end
