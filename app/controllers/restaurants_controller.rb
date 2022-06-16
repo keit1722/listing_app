298a2fabc7f909916581cc22a129de4550f463ab
keit1722 <keit1722@gmail.com>
2022-06-16 17:06:02 +0900

    [add]restaurantの検索用アクションを作成

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,10 +2,10 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants =
       Restaurant
-      .with_attached_images
-      .includes(:restaurant_categories)
-      .page(params[:page])
-      .per(20)
+        .with_attached_images
+        .includes(:restaurant_categories)
+        .page(params[:page])
+        .per(20)
     @restaurant_all = Restaurant.all
     render layout: 'listings_index'
   end
@@ -13,5 +13,24 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.with_attached_images.find_by!(slug: params[:slug])
     render layout: 'listings_single'
+  end
+
+  def search
+    @restaurants =
+      SearchRestaurantsForm
+        .new(search_restaurants_params)
+        .search
+        .with_attached_images
+        .includes(:restaurant_categories)
+        .page(params[:page])
+    @restaurant_all =
+      SearchRestaurantsForm.new(search_restaurants_params).search.all
+    render layout: 'listings_index'
+  end
+
+  private
+
+  def search_restaurants_params
+    params.fetch(:q, {}).permit(:keyword)
   end
 end
