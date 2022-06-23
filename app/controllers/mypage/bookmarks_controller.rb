b19d07c04a393adac01211cd8a0bfb76007c50db
keit1722 <keit1722@gmail.com>
2022-06-23 18:49:55 +0900

    [add]マイページにお気に入り一覧ページを作成

---
+++ app/controllers/mypage/bookmarks_controller.rb
@@ -0,0 +1,12 @@
+class Mypage::BookmarksController < ApplicationController
+  layout 'mypage'
+
+  def index
+    @bookmarks =
+      current_user
+        .bookmarks
+        .includes([:bookmarkable])
+        .page(params[:page])
+        .per(20)
+  end
+end
