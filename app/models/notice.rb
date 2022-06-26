7ada13494a99c5fea337e4ba15676ba6f027a6ca
keit1722 <keit1722@gmail.com>
2022-06-26 15:48:54 +0900

    [add]Noticeモデルを作成

---
+++ app/models/notice.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: notices
+#
+#  id              :bigint           not null, primary key
+#  noticeable_type :string
+#  read            :boolean          default(FALSE), not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  noticeable_id   :bigint
+#  user_id         :bigint
+#
+# Indexes
+#
+#  index_bookmarks_on_user_id_and_noticeable_id_and_type  (user_id,noticeable_id,noticeable_type) UNIQUE
+#  index_notices_on_noticeable_type_and_noticeable_id     (noticeable_type,noticeable_id)
+#  index_notices_on_user_id                               (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+class Notice < ApplicationRecord
+  validates :noticeable_id, uniqueness: { scope: %i[noticeable_type user_id] }
+
+  enum read: { unread: false, read: true }
+end
