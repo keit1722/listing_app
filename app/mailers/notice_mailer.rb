953af6f5c53519e4e86e044a0c4229723f9f27a3
keit1722 <keit1722@gmail.com>
2022-07-01 01:27:26 +0900

    [add]通知があるとメール送信される処理を実装

---
+++ app/mailers/notice_mailer.rb
@@ -0,0 +1,11 @@
+class NoticeMailer < ApplicationMailer
+  def post
+    @user_to = params[:user_to]
+    @post = params[:post]
+
+    mail(
+      to: @user_to.email,
+      subject: "#{@post.postable.name} が新しく投稿をしました",
+    )
+  end
+end
