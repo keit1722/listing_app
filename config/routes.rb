f2a5ca5966c75485db65f262adfc9e4c1acf15a7
keit1722 <keit1722@gmail.com>
2022-07-01 10:42:55 +0900

    [add]ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  resources :users, only: [] do
+    member { get :activate }
+  end
 
   get 'search', to: 'pages#search'
 
