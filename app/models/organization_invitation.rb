995d9303fbcd7448e6b1e9201f88addc4534c337
keit1722 <keit1722@gmail.com>
2022-07-05 16:14:38 +0900

    [add]OrganizaitoionInvitationモデルを作成

---
+++ app/models/organization_invitation.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: organization_invitations
+#
+#  id              :bigint           not null, primary key
+#  accepted        :boolean          default(FALSE), not null
+#  email           :string           not null
+#  expires_at      :datetime         not null
+#  token           :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  inviter_id      :integer          not null
+#  organization_id :bigint
+#
+# Indexes
+#
+#  index_organization_invitations_on_organization_id  (organization_id)
+#  index_organization_invitations_on_token            (token) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (organization_id => organizations.id)
+#
+class OrganizationInvitation < ApplicationRecord
+  validates :email, presence: true
+  validates :inviter_id, presence: true
+  validates :token, presence: true, uniqueness: true
+  validates :expires_at, presence: true
+  validates :accepted, presence: true
+end
