b829d5fc5f9ace4e92444297f9e1eb0390d9d363
keit1722 <keit1722@gmail.com>
2022-07-16 05:01:31 +0900

    [add]反映されていなかったannotateを追加

--- test/factories/organization_invitations.rb
+++ test/factories/organization_invitations.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: organization_invitations
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  expires_at      :datetime         not null
+#  status          :integer          default("untouched"), not null
+#  token           :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  inviter_id      :integer          not null
+#  organization_id :bigint
+#
+# Indexes
+#
+#  index_organization_invitations_on_organization_id  (organization_id)
+#  index_organization_invitations_on_token            (token) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (organization_id => organizations.id)
+#
 FactoryBot.define do
   factory :organization_invitation do
     email { Faker::Internet.unique.email }
