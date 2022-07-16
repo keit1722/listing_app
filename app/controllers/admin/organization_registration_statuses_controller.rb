ca2d3d22e3d870182b7a5f40d1adf7e66b294b5a
keit1722 <keit1722@gmail.com>
2022-07-17 06:11:03 +0900

    [modify]Admin::OrganizationRegistrationStatusesコントローラーの処理を修正

--- app/controllers/admin/organization_registration_statuses_controller.rb
+++ app/controllers/admin/organization_registration_statuses_controller.rb
@@ -1,36 +1,22 @@
 class Admin::OrganizationRegistrationStatusesController < ApplicationController
   layout 'mypage'
 
-  def index
-    @organization_registration_statuses = OrganizationRegistrationStatus.all
-  end
-
-  def new
-    @organization_registration_status = OrganizationRegistrationStatus.new
-  end
-
   def create
     @organization_registration_status =
-      OrganizationRegistrationStatus.new(
-        organization_registration_status_params,
-      )
+      OrganizationRegistration.find(params[:organization_registration_id])
+        .build_organization_registration_status
+    user = @organization_registration_status.organization_registration.user
+    @organization_registration_status.status = params[:status] && user.general?
+    if @organization_registration_status.accepted? && user.business!
+      user.business!
+    end
+
     if @organization_registration_status.save
-      redirect_to organization_registration_status_path,
+      redirect_to admin_organization_registrations_path,
                   success: '申請の回答を作成しました'
     else
-      flash.now[:error] = '申請の回答を作成できませんでした'
-      render :new
+      redirect_to admin_organization_registrations_path,
+                  error: '申請の回答を作成できませんでした'
     end
-  end
-
-  def show
-    @organization_registration_status =
-      OrganizationRegistrationStatus.find(params[:id])
-  end
-
-  private
-
-  def organization_registration_status_params
-    params.require(:organization_registration_status).permit(:status)
   end
 end
