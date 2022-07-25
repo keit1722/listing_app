b61ce17ae4c94126297aafb982ec527afab430b2
keit1722 <keit1722@gmail.com>
2022-07-25 09:50:17 +0900

    [modify]adminに関するルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -78,40 +78,72 @@ Rails.application.routes.draw do
     resources :organizations,
               param: :slug,
               only: %i[index edit update show destroy] do
-      resources :restaurants, param: :slug do
-        resources :posts, module: :restaurants
-      end
-      resources :shops, param: :slug do
-        resources :posts, module: :shops
-      end
-      resources :hotels, param: :slug do
-        resources :posts, module: :hotels
-      end
-      resources :activities, param: :slug do
-        resources :posts, module: :activities
-      end
-      resources :hot_springs, param: :slug do
-        resources :posts, module: :hot_springs
-      end
-      resources :ski_areas, param: :slug do
-        resources :posts, module: :ski_areas
-      end
-      resources :photo_spots, param: :slug do
-        resources :posts, module: :photo_spots
+      scope module: :organizations do
+        resources :restaurants,
+                  param: :slug,
+                  only: %i[index edit update show destroy] do
+          resources :posts,
+                    only: %i[index edit update show destroy],
+                    module: :restaurants
+        end
+        resources :shops,
+                  param: :slug,
+                  only: %i[index edit update show destroy] do
+          resources :posts,
+                    only: %i[index edit update show destroy],
+                    module: :shops
+        end
+        resources :hotels,
+                  param: :slug,
+                  only: %i[index edit update show destroy] do
+          resources :posts,
+                    only: %i[index edit update show destroy],
+                    module: :hotels
+        end
+        resources :activities,
+                  param: :slug,
+                  only: %i[index edit update show destroy] do
+          resources :posts,
+                    only: %i[index edit update show destroy],
+                    module: :activities
+        end
+        resources :hot_springs,
+                  param: :slug,
+                  only: %i[index edit update show destroy] do
+          resources :posts,
+                    only: %i[index edit update show destroy],
+                    module: :hot_springs
+        end
+        resources :ski_areas,
+                  param: :slug,
+                  only: %i[index edit update show destroy] do
+          resources :posts,
+                    only: %i[index edit update show destroy],
+                    module: :ski_areas
+        end
+        resources :photo_spots,
+                  param: :slug,
+                  only: %i[index edit update show destroy] do
+          resources :posts,
+                    only: %i[index edit update show destroy],
+                    module: :photo_spots
+        end
       end
     end
 
     resources :users,
               param: :public_uid,
               only: %i[index edit update show destroy] do
-      resources :bookmarks, only: %i[index]
-      resources :notices, only: %i[index]
-      resources :organization_registrations, only: %i[index show new create]
     end
 
     resources :organization_registrations, only: %i[index show] do
       resources :organization_registration_statuses, only: %i[create]
     end
+  end
+
+  namespace :business do
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
   end
 
   resources :organizations, param: :slug do
