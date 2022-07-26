861a6ae181fc0aa572a31a1c6f5fb206e12a0e47
keit1722 <keit1722@gmail.com>
2022-07-26 23:08:46 +0900

    [add]announcementモデルを作成

---
+++ app/models/announcement.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: announcements
+#
+#  id         :bigint           not null, primary key
+#  body       :text             not null
+#  status     :integer          default("published"), not null
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  poster_id  :integer          not null
+#
+class Announcement < ApplicationRecord
+  validates :title, length: { maximum: 100 }, presence: true
+  validates :body, length: { maximum: 10_000 }, presence: true
+  validates :status, presence: true
+
+  enum status: { published: 1, draft: 2 }
+end
