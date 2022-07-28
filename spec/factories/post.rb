3c81c1813aca609ab7fedf907739d9502cbc18f4
keit1722 <keit1722@gmail.com>
2022-07-28 13:13:55 +0900

    [modify]Postファクトリのカラムの値を変更

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :post do
     title { Faker::Company.unique.name }
-    body { Faker::Company.unique.name }
+    body { Faker::Lorem.paragraph(sentence_count: 10) }
     image do
       Rack::Test::UploadedFile.new('spec/fixtures/fixture.png', 'image/png')
     end
