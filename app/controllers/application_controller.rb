8d6710d17563efc6a7a3c1ebf85bd218e0dbea66
keit1722 <keit1722@gmail.com>
2022-07-30 10:57:10 +0900

    [modify]ログアウト状態でのエラーのリダイレクト先を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   add_flash_types :error, :success, :warning
 
   def not_authenticated
-    redirect_to login_path, error: 'ログインしてください'
+    redirect_to root_path, error: 'ログインしてください'
   end
 
   def require_logout
@@ -10,6 +10,8 @@ class ApplicationController < ActionController::Base
   end
 
   def only_business_or_admin
-    redirect_to root_path, error: 'ビジネスユーザー専用の機能です' if !current_user&.business? && !current_user&.admin?
+    if !current_user&.business? && !current_user&.admin?
+      redirect_to root_path, error: 'ビジネスユーザー専用の機能です'
+    end
   end
 end
