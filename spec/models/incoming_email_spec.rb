53be0cfaf4eefc95268c0a395c2c12fdbca79acd
keit1722 <keit1722@gmail.com>
2022-09-07 15:03:12 +0900

    [add]IncomingEmailに関するファクトリ・モデルspecファイルを作成

---
+++ spec/models/incoming_email_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe IncomingEmail, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+
+# == Schema Information
+#
+# Table name: incoming_emails
+#
+#  id                      :bigint           not null, primary key
+#  announcement            :boolean          default(TRUE), not null
+#  organization            :boolean          default(TRUE), not null
+#  organization_invitation :boolean          default(TRUE), not null
+#  post                    :boolean          default(TRUE), not null
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  user_id                 :bigint
+#
+# Indexes
+#
+#  index_incoming_emails_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
