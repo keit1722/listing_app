6bcec1d692458823e220348e2d398957ab821ee3
keit1722 <keit1722@gmail.com>
2022-10-07 06:13:09 +0900

    [modify]ポリモーフィックの関連先によってincludesの引数が変わるN+1を対策を修正

--- app/controllers/mypage/notices_controller.rb
+++ app/controllers/mypage/notices_controller.rb
@@ -1,11 +1,11 @@
 class Mypage::NoticesController < Mypage::BaseController
   def index
-    @notices =
-      current_user
-      .notices
-      .includes([noticeable: [:postable]])
-      .page(params[:page])
-      .per(20)
-      .ordered
+    notices = current_user.notices.includes([:noticeable])
+    preloader = ActiveRecord::Associations::Preloader.new
+    preloader.preload(
+      notices.select { |notice| notice.noticeable_type == 'Post' },
+      noticeable: [:postable],
+    )
+    @notices = notices.page(params[:page]).per(20).ordered
   end
 end
