2ea3d683b2c9a629cd3ced487df1c92c43782230
keit1722 <keit1722@gmail.com>
2022-10-08 15:39:37 +0900

    [add]oauthsコントローラーの処理を実装

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,7 +1,25 @@
 class OauthsController < ApplicationController
+  skip_before_action :require_login, raise: false
+
   def oauth
+    login_at(params[:provider])
   end
 
   def callback
+    provider = params[:provider]
+    if (@user = login_from(provider))
+      redirect_to root_path, notice: "#{provider.titleize}でログインしました"
+    else
+      begin
+        @user = create_from(provider)
+        @user.activate!
+        reset_session
+        auto_login(@user)
+        redirect_to root_path, notice: "#{provider.titleize}でログインしました"
+      rescue StandardError
+        redirect_to root_path,
+                    alert: "#{provider.titleize}でのログインに失敗しました"
+      end
+    end
   end
 end
