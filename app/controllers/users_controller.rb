48b87f811b24f7863e1ab83271ca689877a8b68d
keit1722 <keit1722@gmail.com>
2022-10-10 16:16:35 +0900

    [modify]if文の条件をかっこで囲んだ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   end
 
   def activate
-    if @user = User.load_from_activation_token(params[:id])
+    if (@user = User.load_from_activation_token(params[:id]))
       @user.activate!
       auto_login(@user)
       redirect_to root_path, success: 'アカウントが有効になりました'
