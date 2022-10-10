a25c84fbaec8a61daee5575f5b849f37624c8b32
keit1722 <keit1722@gmail.com>
2022-10-10 16:12:39 +0900

    [modify]フラッシュメッセージの色を変更

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -8,17 +8,17 @@ class OauthsController < ApplicationController
   def callback
     provider = auth_params[:provider]
     if (@user = login_from(provider))
-      redirect_to root_path, notice: "#{provider.titleize}でログインしました"
+      redirect_to root_path, success: "#{provider.titleize}でログインしました"
     else
       begin
         @user = create_from(provider)
         @user.activate!
         reset_session
         auto_login(@user)
-        redirect_to root_path, notice: "#{provider.titleize}でログインしました"
+        redirect_to root_path, success: "#{provider.titleize}でログインしました"
       rescue StandardError
         redirect_to root_path,
-                    alert: "#{provider.titleize}でのログインに失敗しました"
+                    error: "#{provider.titleize}でのログインに失敗しました"
       end
     end
   end
