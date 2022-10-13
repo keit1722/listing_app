dd3b95c87eb069534508c1fdac1fba4fedf5f4d1
keit1722 <keit1722@gmail.com>
2022-10-14 01:38:13 +0900

    [modify]環境によって読み込まれるseedファイルを変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,1 @@
-require './db/seeds/restaurant_categories'
-require './db/seeds/shop_categories'
-require './db/seeds/districts'
+require "./db/seeds/environment/#{Rails.env.downcase}.rb"
---
+++ db/seeds/environment/development.rb
@@ -0,0 +1,4 @@
+require './db/seeds/restaurant_categories'
+require './db/seeds/shop_categories'
+require './db/seeds/districts'
+require './db/seeds/dummy_data'
