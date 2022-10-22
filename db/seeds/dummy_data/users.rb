a3378aab91ac81c5303cb7eb57c97a7247dd32e6
keit1722 <keit1722@gmail.com>
2022-10-23 05:29:22 +0900

    [modify]Userダミー作成処理コードを修正

--- db/seeds/dummy_data/users.rb
+++ db/seeds/dummy_data/users.rb
@@ -4,34 +4,28 @@ users = [
     first_name: '太郎',
     username: 'ゲストユーザー',
     email: 'guest@example.com',
-    role: :general
+    role: :general,
   },
   {
     last_name: '佐藤',
     first_name: '次郎',
     username: 'アドミンユーザー',
     email: 'admin@example.com',
-    role: :admin
+    role: :admin,
   },
   {
     last_name: '鈴木',
     first_name: '三郎',
     username: 'ビジネスユーザー',
     email: 'business@example.com',
-    role: :business
-  }
+    role: :business,
+  },
 ]
 
-users.each_with_index do |user, _index|
+users.each do |user|
   created_user =
     User.create(
-      last_name: user[:last_name],
-      first_name: user[:first_name],
-      username: user[:username],
-      email: user[:email],
-      role: user[:role],
-      password: 'password',
-      password_confirmation: 'password'
+      user.merge({ password: 'password', password_confirmation: 'password' }),
     )
   created_user.activate!
 end
@@ -45,7 +39,7 @@ end
       email: Faker::Internet.unique.safe_email,
       password: 'password',
       password_confirmation: 'password',
-      role: :business
+      role: :business,
     )
   user.activate!
 end
