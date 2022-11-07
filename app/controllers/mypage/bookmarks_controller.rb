68fa7a2989db689276f8efc3e9f143c386e16ea6
keit1722 <keit1722@gmail.com>
2022-11-08 02:03:43 +0900

    [modify]BookmarkのN+1対策

--- app/controllers/mypage/bookmarks_controller.rb
+++ app/controllers/mypage/bookmarks_controller.rb
@@ -1,6 +1,10 @@
 class Mypage::BookmarksController < Mypage::BaseController
   def index
     @bookmarks =
-      current_user.bookmarks.includes(:bookmarkable).page(params[:page]).per(20)
+      current_user
+        .bookmarks
+        .includes(bookmarkable: [main_image_attachment: :blob])
+        .page(params[:page])
+        .per(20)
   end
 end
