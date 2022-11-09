19da882eeddb055a0426b2aec84d443317559635
keit1722 <keit1722@gmail.com>
2022-11-09 22:45:34 +0900

    [mofidy]インデントの修正

--- app/controllers/mypage/bookmarks_controller.rb
+++ app/controllers/mypage/bookmarks_controller.rb
@@ -2,9 +2,9 @@ class Mypage::BookmarksController < Mypage::BaseController
   def index
     @bookmarks =
       current_user
-        .bookmarks
-        .includes(bookmarkable: [main_image_attachment: :blob])
-        .page(params[:page])
-        .per(20)
+      .bookmarks
+      .includes(bookmarkable: [main_image_attachment: :blob])
+      .page(params[:page])
+      .per(20)
   end
 end
