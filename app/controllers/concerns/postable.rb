f628aaf6225ec96a4ea01964b48e29c5dbf219ce
keit1722 <keit1722@gmail.com>
2022-11-11 03:06:08 +0900

    [modify]postsを3つに絞り込むことをインスタンス変数ではなくビューにて実装

--- app/controllers/concerns/postable.rb
+++ app/controllers/concerns/postable.rb
@@ -11,13 +11,11 @@ module Postable
         .page(params[:page])
         .per(5)
         .ordered
-    @three_posts = @posts.limit(3)
   end
 
   def show
     @post = @postable.posts.published.find(params[:id])
     @posts = @postable.posts.with_attached_image.published.ordered
-    @three_posts = @posts.limit(3)
   end
 
   private
